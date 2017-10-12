1553ef96dc3ff6baa89cb4424c1854fce365a0ca
Yosua1011 <yosua1011@gmail.com>
2017-10-12 13:24:42 +0700

    models generated

---
+++ server/models/Article.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const articleSchema = new Schema({
+  slug: {type: String},
+  title: {type: String, required: true},
+  article: {type: String, required: true},
+  author: String
+}, {
+  timestamps: true
+})
+
+const Article = mongoose.model('Article', articleSchema)
+
+module.exports = Article
